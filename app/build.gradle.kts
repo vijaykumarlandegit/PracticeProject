c7ef507a92360e6457e1ce11f66ab0a64b7ba0bd
Vijaykumar Lande <vijaykumar70287606@gmail.com>
2025-09-07 12:37:40 +0530

    after android studio update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.practice.practiceproject"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.practice.practiceproject"
